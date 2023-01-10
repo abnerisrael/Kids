2bb6a9bfa810b7020e2532e4ffb2c7418a289002
Abner Israel <abnerisrael.a@gmail.com>
2023-01-10 00:38:17 -0300

    Adicionado navegacao com react navigation e configurado rotas e menu bottom

--- App.js
+++ App.js
@@ -10,12 +10,21 @@ import React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
 import BottomMenuBar from './src/ui/common/BottomMenuBar';
 import AgendaView from './src/ui/agenda/AgendaView';
+import EquipeView from './src/ui/equipe/EquipeView';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <PaperProvider>
-      <AgendaView />
-      <BottomMenuBar />
+      <NavigationContainer>
+        <Tab.Navigator tabBar={props => <BottomMenuBar {...props} />}>
+          <Tab.Screen name="Agenda" component={AgendaView} />
+          <Tab.Screen name="Equipe" component={EquipeView} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 };
---
+++ src/ui/equipe/EquipeView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Text} from 'react-native-paper';
+
+const EquipeView = () => {
+  return (
+    <View>
+      <Text>Sua Equipe</Text>
+    </View>
+  );
+};
+
+export default EquipeView;
