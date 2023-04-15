38c8b861b56b4e649a7de79a086bd1e089a6eda3
Abner Israel <abnerisrael.agmail.coml>
2023-04-15 17:05:48 -0300

    Criado logica inicial para adicao de equipe

--- src/ui/evento/CreateEvent.view.tsx
+++ src/ui/evento/CreateEvent.view.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Divider, Text, Chip, Button} from 'react-native-paper';
+import React, {useState} from 'react';
+import {Divider, Text, Chip, Button, Avatar} from 'react-native-paper';
 import {
   View,
   StyleSheet,
@@ -10,6 +10,7 @@ import {
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {useDateTimerPicker} from '../hooks/useDateTimePicker';
 import {useForm, Controller} from 'react-hook-form';
+import {ScrollView} from 'react-native-gesture-handler';
 
 type FormData = {
   tema: string;
@@ -21,11 +22,37 @@ type FormData = {
   descricao: string;
 };
 
+type Voluntario = {
+  id: number;
+  nome: string;
+  funcao: Array<string>;
+};
+
 const CreateEventView = () => {
   const dateStartsPicker = useDateTimerPicker();
   const dateEndsPicker = useDateTimerPicker();
   const timeStartsPicker = useDateTimerPicker();
   const timeEndsPicker = useDateTimerPicker();
+  const [professores, setProfessores] = useState<Voluntario[]>([]);
+  const [auxiliares, setAuxiliares] = useState<Voluntario[]>([]);
+
+  const onAddProfessor = () => {
+    const professor: Voluntario = {
+      id: Math.random() * 1,
+      nome: 'teacher',
+      funcao: ['professor'],
+    };
+    setProfessores([...professores, professor]);
+  };
+
+  const onAddAuxiliar = () => {
+    const auxiliar: Voluntario = {
+      id: Math.random() * 1,
+      nome: 'teacher',
+      funcao: ['auxiliar'],
+    };
+    setAuxiliares([...auxiliares, auxiliar]);
+  };
 
   const {
     control,
@@ -45,160 +72,204 @@ const CreateEventView = () => {
 
   return (
     <KeyboardAvoidingView style={stylesModal.viewForm}>
-      <Controller
-        control={control}
-        rules={{
-          required: true,
-        }}
-        name="tema"
-        render={({field: {onChange, onBlur, value}}) => (
-          <TextInput
-            style={stylesModal.title}
-            placeholder="Tema do evento ..."
-            placeholderTextColor="#030303"
-            cursorColor="#000"
-            selectionColor="#000"
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-          />
-        )}
-      />
-      {errors.tema && <Text style={{color: 'red'}}>Preencha este campo.</Text>}
-
-      <Divider style={stylesModal.divider} />
-      <View style={{marginBottom: 20}}>
-        <Text variant="bodyLarge" style={stylesModal.label}>
-          Início
-        </Text>
-        <View style={{...stylesModal.row, justifyContent: 'space-between'}}>
-          <TouchableOpacity onPress={dateStartsPicker.showDatepicker}>
-            <Text variant="bodyLarge" style={stylesModal.label}>
-              {dateStartsPicker.getDate().format('ddd. D MMMM YYYY')}
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={timeStartsPicker.showTimepicker}>
-            <Text variant="bodyLarge" style={stylesModal.label}>
-              {timeStartsPicker.getDate().format('H:mm')}
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View>
-        <Text variant="bodyLarge" style={stylesModal.label}>
-          Término
-        </Text>
-        <View style={{...stylesModal.row, justifyContent: 'space-between'}}>
-          <TouchableOpacity onPress={dateEndsPicker.showDatepicker}>
-            <Text variant="bodyLarge" style={stylesModal.label}>
-              {dateEndsPicker.getDate().format('ddd. D MMMM YYYY')}
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={timeEndsPicker.showTimepicker}>
-            <Text variant="bodyLarge" style={stylesModal.label}>
-              {timeEndsPicker.getDate().format('H:mm')}
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <Divider style={stylesModal.divider} />
-      <View style={stylesModal.row}>
-        <Icon
-          name="account-group"
-          size={20}
-          color="#000"
-          style={stylesModal.iconSection}
-        />
-        <Text variant="bodyLarge" style={stylesModal.label}>
-          Equipe
-        </Text>
-      </View>
-      <Divider style={stylesModal.divider} />
-      <View style={stylesModal.row}>
-        <Icon
-          name="door-closed"
-          size={20}
-          color="#000"
-          style={stylesModal.iconSection}
-        />
-        <Text variant="bodyLarge" style={stylesModal.label}>
-          Sala
-        </Text>
-      </View>
-      {errors.sala && <Text style={{color: 'red'}}>Preencha este campo.</Text>}
-      <View
-        style={{
-          ...stylesModal.row,
-          marginTop: 10,
-          flexWrap: 'wrap',
-          flexDirection: 'row',
-        }}>
+      <ScrollView>
         <Controller
           control={control}
           rules={{
             required: true,
           }}
-          name="sala"
-          render={({field: {value}}) => {
-            return ['Babies', 'Kids', 'Juniores', 'Todos', 'Nenhum'].map(
-              sala => {
-                return (
-                  <Chip
-                    key={sala}
-                    icon={sala === value ? 'check' : ''}
-                    style={{marginRight: 10, marginBottom: 10}}
-                    onPress={() => {
-                      setValue('sala', sala);
-                    }}>
-                    {sala}
-                  </Chip>
-                );
-              },
-            );
-          }}
+          name="tema"
+          render={({field: {onChange, onBlur, value}}) => (
+            <TextInput
+              style={stylesModal.title}
+              placeholder="Tema do evento ..."
+              placeholderTextColor="#030303"
+              cursorColor="#000"
+              selectionColor="#000"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
         />
-      </View>
-      <Divider style={stylesModal.divider} />
-      <View style={stylesModal.row}>
-        <Icon
-          name="text"
-          size={20}
-          color="#000"
-          style={stylesModal.iconSection}
-        />
-        <Text variant="bodyLarge" style={stylesModal.label}>
-          Descrição
-        </Text>
-      </View>
-      {errors.descricao && (
-        <Text style={{color: 'red'}}>Preencha este campo.</Text>
-      )}
-      <Controller
-        control={control}
-        rules={{
-          required: true,
-        }}
-        name="descricao"
-        render={({field: {onChange, onBlur, value}}) => (
-          <TextInput
-            style={stylesModal.textArea}
-            cursorColor="#000"
-            selectionColor="#000"
-            editable
-            multiline
-            numberOfLines={4}
-            maxLength={255}
-            placeholderTextColor="#000"
-            placeholder="Escreva uma descrição ..."
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-          />
+        {errors.tema && (
+          <Text style={{color: 'red'}}>Preencha este campo.</Text>
         )}
-      />
-      <Button mode="contained-tonal" onPress={handleSubmit(onSubmit)}>
-        Salvar
-      </Button>
+
+        <Divider style={stylesModal.divider} />
+        <View style={{marginBottom: 20}}>
+          <Text variant="bodyLarge" style={stylesModal.label}>
+            Início
+          </Text>
+          <View style={{...stylesModal.row, justifyContent: 'space-between'}}>
+            <TouchableOpacity onPress={dateStartsPicker.showDatepicker}>
+              <Text variant="bodyLarge" style={stylesModal.label}>
+                {dateStartsPicker.getDate().format('ddd. D MMMM YYYY')}
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={timeStartsPicker.showTimepicker}>
+              <Text variant="bodyLarge" style={stylesModal.label}>
+                {timeStartsPicker.getDate().format('H:mm')}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View>
+          <Text variant="bodyLarge" style={stylesModal.label}>
+            Término
+          </Text>
+          <View style={{...stylesModal.row, justifyContent: 'space-between'}}>
+            <TouchableOpacity onPress={dateEndsPicker.showDatepicker}>
+              <Text variant="bodyLarge" style={stylesModal.label}>
+                {dateEndsPicker.getDate().format('ddd. D MMMM YYYY')}
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={timeEndsPicker.showTimepicker}>
+              <Text variant="bodyLarge" style={stylesModal.label}>
+                {timeEndsPicker.getDate().format('H:mm')}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <Divider style={stylesModal.divider} />
+        <View style={stylesModal.row}>
+          <Icon
+            name="account-group"
+            size={20}
+            color="#000"
+            style={stylesModal.iconSection}
+          />
+          <Text variant="bodyLarge" style={stylesModal.label}>
+            Equipe
+          </Text>
+        </View>
+        <View style={stylesModal.row}>
+          <Text variant="bodyLarge" style={stylesModal.label}>
+            Professores
+          </Text>
+        </View>
+        <View style={stylesModal.avatarsRow}>
+          {professores.map((professor: Voluntario) => (
+            <TouchableOpacity key={professor.id}>
+              <Avatar.Image
+                style={stylesModal.avatar}
+                size={48}
+                source={require('../../assets/img/cintia.jpeg')}
+              />
+            </TouchableOpacity>
+          ))}
+          <TouchableOpacity onPress={onAddProfessor}>
+            <Avatar.Icon style={stylesModal.avatar} size={48} icon="plus" />
+          </TouchableOpacity>
+        </View>
+        <View style={stylesModal.row}>
+          <Text variant="bodyLarge" style={stylesModal.label}>
+            Auxiliares
+          </Text>
+        </View>
+        <View style={stylesModal.avatarsRow}>
+          {auxiliares.map((auxiliar: Voluntario) => (
+            <TouchableOpacity key={auxiliar.id}>
+              <Avatar.Image
+                style={stylesModal.avatar}
+                size={48}
+                source={require('../../assets/img/cintia.jpeg')}
+              />
+            </TouchableOpacity>
+          ))}
+          <TouchableOpacity onPress={onAddAuxiliar}>
+            <Avatar.Icon style={stylesModal.avatar} size={48} icon="plus" />
+          </TouchableOpacity>
+        </View>
+        <Divider style={stylesModal.divider} />
+        <View style={stylesModal.row}>
+          <Icon
+            name="door-closed"
+            size={20}
+            color="#000"
+            style={stylesModal.iconSection}
+          />
+          <Text variant="bodyLarge" style={stylesModal.label}>
+            Sala
+          </Text>
+        </View>
+        {errors.sala && (
+          <Text style={{color: 'red'}}>Preencha este campo.</Text>
+        )}
+        <View
+          style={{
+            ...stylesModal.row,
+            marginTop: 10,
+            flexWrap: 'wrap',
+            flexDirection: 'row',
+          }}>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            name="sala"
+            render={({field: {value}}) => {
+              return ['Babies', 'Kids', 'Juniores', 'Todos', 'Nenhum'].map(
+                sala => {
+                  return (
+                    <Chip
+                      key={sala}
+                      icon={sala === value ? 'check' : ''}
+                      style={{marginRight: 10, marginBottom: 10}}
+                      onPress={() => {
+                        setValue('sala', sala);
+                      }}>
+                      {sala}
+                    </Chip>
+                  );
+                },
+              );
+            }}
+          />
+        </View>
+        <Divider style={stylesModal.divider} />
+        <View style={stylesModal.row}>
+          <Icon
+            name="text"
+            size={20}
+            color="#000"
+            style={stylesModal.iconSection}
+          />
+          <Text variant="bodyLarge" style={stylesModal.label}>
+            Descrição
+          </Text>
+        </View>
+        {errors.descricao && (
+          <Text style={{color: 'red'}}>Preencha este campo.</Text>
+        )}
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          name="descricao"
+          render={({field: {onChange, onBlur, value}}) => (
+            <TextInput
+              style={stylesModal.textArea}
+              cursorColor="#000"
+              selectionColor="#000"
+              editable
+              multiline
+              numberOfLines={4}
+              maxLength={255}
+              placeholderTextColor="#000"
+              placeholder="Escreva uma descrição ..."
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+        />
+        <Button mode="contained-tonal" onPress={handleSubmit(onSubmit)}>
+          Salvar
+        </Button>
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 };
@@ -259,6 +330,11 @@ const stylesModal = StyleSheet.create({
   row: {flexDirection: 'row'},
   iconSection: {marginRight: 5},
   label: {color: '#000'},
+  avatar: {marginRight: 10, marginTop: 5, marginBottom: 5},
+  avatarsRow: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
 });
 
 export default CreateEventView;
